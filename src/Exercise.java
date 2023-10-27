87a708c98a20f6322f50ad553b2d38ddf4c4f6cc
Aditya Gupta <adityagupta3011@gmail.com>
2023-10-27 20:46:21 +0530

    Add existing project files to Git

---
+++ src/Exercise.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Aditya
+ */
+public class Exercise {
+    private String name;
+    private String link;
+
+    public Exercise(String name, String link) {
+        this.name = name;
+        this.link = link;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getLink() {
+        return link;
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
+}
---
+++ src/ex_database.java
@@ -0,0 +1,46 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Aditya
+ */
+import java.util.ArrayList;
+import java.util.List;
+public class ex_database {
+    
+}
+class FileNode {
+    private String name;
+    private List<FileNode> children;
+    private Exercise exercise;
+
+    public FileNode(String name) {
+        this.name = name;
+        this.children = new ArrayList<>();
+        this.exercise = null;
+    }
+
+    public void addExercise(Exercise exercise) {
+        this.exercise = exercise;
+    }
+
+    public void addChild(FileNode child) {
+        children.add(child);
+    }
+
+    public List<FileNode> getChildren() {
+        return children;
+    }
+
+    public Exercise getExercise() {
+        return exercise;
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
+}
